3a088122b57154583fcd3b2c81e112164895f756
jude-mok <sm10732@nyu.edu>
2026-04-12 22:14:09 -0400

    fix(router): update predict endpoint to use team1/team2 parameters

--- backend/app/routers/predict.py
+++ backend/app/routers/predict.py
@@ -12,14 +12,14 @@ def teams():
 def predict(request: Predict_Request):
     teams = get_teams()
     
-    if request.blue_team not in teams:
-        raise HTTPException(status_code=400, detail=f"{request.blue_team} can't find the team")
-    if request.red_team not in teams:
-        raise HTTPException(status_code=400, detail=f"{request.red_team} can't find the team")
-    if request.blue_team == request.red_team:
+    if request.team1 not in teams:
+        raise HTTPException(status_code=400, detail=f"{request.team1} can't find the team")
+    if request.team2 not in teams:
+        raise HTTPException(status_code=400, detail=f"{request.team2} can't find the team")
+    if request.team1 == request.team2:
         raise HTTPException(status_code=400, detail="can't chose the same team to predict")
-    
-    return predict_match(request.blue_team, request.red_team)
+
+    return predict_match(request.team1, request.team2)
 
 @router.get("/features")
 def features():
